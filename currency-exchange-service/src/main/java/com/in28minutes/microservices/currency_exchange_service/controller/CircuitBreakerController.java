97a2cb177bf98110239637fda907f5d1c22e9971
Yash Kambaria <yashkambaria245@gmail.com>
2025-09-20 18:45:40 +0530

    Added the Retry with default

---
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/controller/CircuitBreakerController.java
@@ -0,0 +1,20 @@
+package com.in28minutes.microservices.currency_exchange_service.controller;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+public class CircuitBreakerController {
+	@GetMapping("/get")
+	@Retry(name = "default")
+	public String get(){
+		log.info("-----------------> Attempted to get data");
+		ResponseEntity<String> entity = new RestTemplate().getForEntity("http://localhost:8010/get", String.class);
+		return entity.getBody();
+	}
+}
