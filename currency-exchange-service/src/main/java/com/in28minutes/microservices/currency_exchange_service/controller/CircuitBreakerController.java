e6a8f3ab62c96e34397c89c49832ff552805e019
Yash Kambaria <yashkambaria245@gmail.com>
2025-09-20 19:43:21 +0530

    Added CircuitBreaker

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/controller/CircuitBreakerController.java
@@ -1,5 +1,6 @@
 package com.in28minutes.microservices.currency_exchange_service.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,8 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class CircuitBreakerController {
 	@GetMapping("/get")
-	@Retry(name = "sample-api",fallbackMethod = "hardcodeResponse")
+//	@Retry(name = "sample-api",fallbackMethod = "hardcodeResponse")
+	@CircuitBreaker(name = "default",fallbackMethod = "hardcodeResponse")
 	public String get(){
 		log.info("-----------------> Attempted to get data");
 		ResponseEntity<String> entity = new RestTemplate().getForEntity("http://localhost:8010/get", String.class);
